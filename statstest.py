0b9e6ce96d32b66f9fb81ca986dbc6bb5a80c000
4aYnOmAn <83335375+nothingISchay@users.noreply.github.com>
2021-08-19 20:42:09 +0500

    Update statstest.py

--- statstest.py
+++ statstest.py
@@ -1,50 +1,50 @@
 from tkinter import *
-import asyncio
 
 # Окно
 window = Tk()
-window.title("Stats")
+window.title('Stats')
 window.geometry('150x550')
 
 # Не обращать внимания на исполнение до последнего!!
 # Высчитывает и изменяет переменную бонусных поинтов функ. хар. исходя кол-ва числа поинтов функ. хар.
 ## Переменная int_points: переменная числа поинтов из функ. хар.
 ## Переменная bonus_points: переменная печатаемых бонусных поинтов в окне из функ. хар.
-async def bonus_calc(int_points, bonus_points):
+def bonus_calc(int_points):
     if int_points == 1:
-        bonus_points.configure(text="[ -5 ]")
+        new_bonus_points = '[ -5 ]'
     elif 1 < int_points < 4:
-        bonus_points.configure(text="[ -4 ]")
+        new_bonus_points = '[ -4 ]'
     elif 3 < int_points < 6:
-        bonus_points.configure(text="[ -3 ]")
+        new_bonus_points = '[ -3 ]'
     elif 5 < int_points < 8:
-        bonus_points.configure(text="[ -2 ]")
+        new_bonus_points = '[ -2 ]'
     elif 7 < int_points < 10:
-        bonus_points.configure(text="[ -1 ]")
+        new_bonus_points = '[ -1 ]'
     elif 9 < int_points < 12:
-        bonus_points.configure(text="[ +0 ]")
+        new_bonus_points = '[ +0 ]'
     elif 11 < int_points < 14:
-        bonus_points.configure(text="[ +1 ]")
+        new_bonus_points = '[ +1 ]'
     elif 13 < int_points < 16:
-        bonus_points.configure(text="[ +2 ]")
+        new_bonus_points = '[ +2 ]'
     elif 15 < int_points < 18:
-        bonus_points.configure(text="[ +3 ]")
+        new_bonus_points = '[ +3 ]'
     elif 17 < int_points < 20:
-        bonus_points.configure(text="[ +4 ]")
+        new_bonus_points = '[ +4 ]'
     elif 19 < int_points < 22:
-        bonus_points.configure(text="[ +5 ]")
+        new_bonus_points = '[ +5 ]'
     elif 21 < int_points < 24:
-        bonus_points.configure(text="[ +6 ]")
+        new_bonus_points = '[ +6 ]'
     elif 23 < int_points < 26:
-        bonus_points.configure(text="[ +7 ]")
+        new_bonus_points = '[ +7 ]'
     elif 25 < int_points < 28:
-        bonus_points.configure(text="[ +8 ]")
+        new_bonus_points = '[ +8 ]'
     elif 27 < int_points < 30:
-        bonus_points.configure(text="[ +9 ]")
+        new_bonus_points = '[ +9 ]'
     elif int_points == 30:
-        bonus_points.configure(text="[ +10 ]")
+        new_bonus_points = '[ +10 ]'
     else:
-        bonus_points.configure(text="!!!err!!!")
+        new_bonus_points = '!!!err!!!'
+    return new_bonus_points
 
 # Общие кнопочки
 ## Переменная action: '+' или '-'
@@ -52,47 +52,44 @@ async def bonus_calc(int_points, bonus_points):
 ## Переменная str_points: переменная печатаемых поинтов в окне из функ. хар.
 ## Переменная bonus_points: переменная печатаемых бонусных поинтов в окне из функ. хар.
 ## Функ. bonus_calc высчитывает и изменяет переменную бонусных поинтов функ. хар. исходя кол-ва числа поинтов функ. хар.
-async def score_btn(action, int_points, str_points, bonus_points):
-    if action == '+':
-        if int_points < 30:
-            int_points += 1
-            str_points.configure(text=int_points)
-            await bonus_calc(int_points=int_points, bonus_points=bonus_points)
-    elif action == '-':
+def score_btn(action, int_points, str_points, bonus_points):
+    if action == '-':
         if int_points > 1:
             int_points -= 1
             str_points.configure(text=int_points)
-            await bonus_calc(int_points=int_points, bonus_points=bonus_points)
+            bonus_points.configure(text=bonus_calc(int_points=int_points))
+    elif action == '+':
+        if int_points < 30:
+            int_points += 1
+            str_points.configure(text=int_points)
+            bonus_points.configure(text=bonus_calc(int_points=int_points))
     else:
         raise SyntaxError
 
 # Функ. для создания функ. хар.
-async def score(row, str_name):
+def score(row, str_name):
     ## int_points
     int_points=1
     ##  Название
-    name = Label(window, text=f' {str_name}', font=("System", 10))
+    name = Label(window, text=f' {str_name}', font=('System', 10))
     name.grid(column=1, row=row)
     ## bonus_points
-    bonus_points = Label(window, text="[ -5 ]", font=("System", 20))
+    bonus_points = Label(window, text='[ -5 ]', font=('System', 20))
     bonus_points.grid(column=1, row=row+1)
     ## str_points
-    str_points = Label(window, text=int_points, font=("System", 7))
+    str_points = Label(window, text=int_points, font=('System', 7))
     str_points.grid(column=1, row=row+2)
     ## Кнопачки
-    btn_one = Button(window, text="-", command=await score_btn(action='-', int_points=int_points, str_points=str_points, bonus_points=bonus_points), font=("System", 7))
+    btn_one = Button(window, text='-', command=score_btn(action='-', int_points=int_points, str_points=str_points, bonus_points=bonus_points), font=('System', 7))
     btn_one.grid(column=0, row=row+2)
-    btn_two = Button(window, text="+", command=await score_btn(action='+', int_points=int_points, str_points=str_points, bonus_points=bonus_points), font=("System", 7))
+    btn_two = Button(window, text='+', command=score_btn(action='+', int_points=int_points, str_points=str_points, bonus_points=bonus_points), font=('System', 7))
     btn_two.grid(column=2, row=row+2)
 
-
-async def stats():
+def stats(names):
     row = 0
-    await score(row=row, str_name='Charisma')
-    row += 3
-    await score(row=row, str_name='Huisma')
+    for str_name in names:
+        score(row=row, str_name=str_name)
+        row += 3
 
-
-loop = asyncio.get_event_loop()
-loop.run_until_complete(stats())
+stats(['Strength', 'Dexterity', 'Constitution', 'Intelligence', 'Wisdom', 'Charisma'])
 window.mainloop()
